d49a19815da9edea78e3810b0b7eeb500ba1ea8a
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-04 20:25:19 +0530

    Add home

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import PageHeading from "./PageHeading";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <PageHeading  title="Explore Eazy Stickers!">
+        <p className="page-heading-paragraph">
+          Add a touch of creativity to your space with our wide range of fun and
+          unique stickers. Perfect for any occasion!
+        </p>
+      </PageHeading>
+    </div>
+  );
+}
