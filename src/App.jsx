f46f61102c236f5b4914544ed0b4295a4bdaf5b5
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-04 20:26:33 +0530

    Add Home and Footer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Footer  from './components/footer/Footer'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
