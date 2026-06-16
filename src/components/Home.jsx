2fa80475b17bd325f3797080d025a1a015b11755
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-16 18:54:36 +0530

    Add page loader and error handler for getting products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,53 @@
 import PageHeading from "./PageHeading";
-import  products  from "../data/products";
 import ProductListing from "./ProductListing";
 import apiClient from "../api/apiClient";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetchProducts();
-      }, []);
-      
+  }, []);
+
   const fetchProducts = async () => {
     try {
-      const response = await apiClient.get("/products");    
+      setLoading(true);
+      const response = await apiClient.get("/products");
       setProducts(response.data);
     } catch (error) {
-      console.error("Error fetching products:", error);
-    }  };
+      setError(
+        error.response?.data?.message ||
+          "Failed to fetch products. Please try again later.",
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl font-semibold">Loading products...</span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="text-xl text-red-500">Error: {error}</span>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-6xl mx-auto px-6 py-8">
-      <PageHeading  title="Explore Eazy Stickers!">
-          Add a touch of creativity to your space with our wide range of fun and
-          unique stickers. Perfect for any occasion!
+      <PageHeading title="Explore Eazy Stickers!">
+        Add a touch of creativity to your space with our wide range of fun and
+        unique stickers. Perfect for any occasion!
       </PageHeading>
       <ProductListing products={products} />
     </div>
