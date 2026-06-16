fe4c2ae5862724f6c640d836bfc7fec8a1e9d048
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-16 17:57:39 +0530

    Implement get Products from backend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,25 @@
 import PageHeading from "./PageHeading";
 import  products  from "../data/products";
 import ProductListing from "./ProductListing";
+import apiClient from "../api/apiClient";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+      }, []);
+      
+  const fetchProducts = async () => {
+    try {
+      const response = await apiClient.get("/products");    
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }  };
+
   return (
     <div className="max-w-6xl mx-auto px-6 py-8">
       <PageHeading  title="Explore Eazy Stickers!">
