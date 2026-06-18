f79aa598ad66e25039204b9c5fde80c88a26a9b1
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-18 18:44:33 +0530

    Implement search and sort functionality

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,22 +1,74 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import React, { useState } from "react";
+import ProductCard from "./ProductCard";
+import SearchBox from "./SearchBox";
+import Dropdown from "./Dropdown";
+
+const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 function ProductListing({ products }) {
+  const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState("Popularity");
+
+   const filteredAndSortedProducts = useMemo(() => {
+    if (!Array.isArray(products)) {
+      return [];
+    }
+
+    let filteredProducts = products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return filteredProducts.slice().sort((a, b) => {
+      switch (selectedSort) {
+        case "Price Low to High":
+          return parseFloat(a.price) - parseFloat(b.price);
+        case "Price High to Low":
+          return parseFloat(b.price) - parseFloat(a.price);
+        case "Popularity":
+        default:
+          return parseInt(b.popularity) - parseInt(a.popularity);
+      }
+    });
+  }, [products, searchText, selectedSort]);
+
+  function handleSearchChange(inputSearch) {
+    setSearchText(inputSearch);
+  }
+
+  function handleSortChange(sortType) {
+    setSelectedSort(sortType);
+  }
+
+
   return (
     <div className="max-w-6xl mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12"> 
-            {
-                products.length > 0 ?(
-                    products.map((product) => (
-                        <ProductCard key={product.productId} product={product} />
-                    ))
-                ):(
-                    <p className="products-listing-empty">No products available.</p>
-                )
-            }
-        </div>
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox
+          label="Search:"
+          placeholder="Search Products..."
+          value={searchText}
+          handleSearch={handleSearchChange}
+        />
+        <Dropdown
+          label="Sort By:"
+          options={sortList}
+          value={selectedSort}
+          handleSort={handleSortChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
+        {filteredAndSortedProducts.length > 0 ? (
+          filteredAndSortedProducts.map((product) => (
+            <ProductCard key={product.productId} product={product} />
+          ))
+        ) : (
+          <p className="products-listing-empty">No products available.</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductListing
+export default ProductListing;
