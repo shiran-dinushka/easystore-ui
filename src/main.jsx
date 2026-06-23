7ca4bd0af842a63e7d28ab0c556a850098aa5e49
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-23 17:20:22 +0530

    Chnage the route implemtation and add error elemnt

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
 import Login from './components/Login.jsx';
 import Cart from './components/Cart.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
 
-const router = createBrowserRouter([
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+/*const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -40,7 +55,7 @@ const router = createBrowserRouter([
       }
     ],
   },
-]);
+]);*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
