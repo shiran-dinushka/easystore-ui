62ed18d365200727c768d8d4208f584670275224
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-23 18:28:04 +0530

    Add useNavigation for product loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import './App.css'
 import Header from './components/Header'
 import Footer  from './components/footer/Footer'
 import Home from './components/Home'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigation = useNavigation();
 
   return (
     <>
       <Header />
-      <Outlet />
+      {navigation.state === "loading" ?(
+        <div className="flex items-center justify-center min-h-213 ">
+          <span className="text-4xl font-semibold text-primary dark:text-light">Loading...</span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </>
   )
