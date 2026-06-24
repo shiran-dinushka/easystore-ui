340300ae5970dab0249650e1c99b7a2af6461960
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-24 20:22:10 +0530

    Add contactAction for submitting form and dynamic routes for product items

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
-import Home from './components/Home.jsx';
-import About from './components/About.jsx';
-import Contact from './components/Contact.jsx';
-import Login from './components/Login.jsx';
-import Cart from './components/Cart.jsx';
-import ErrorPage from './components/ErrorPage.jsx';
-import { productsLoader } from './components/Home.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact, { contactAction } from "./components/Contact.jsx";
+import Login from "./components/Login.jsx";
+import Cart from "./components/Cart.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+import { productsLoader } from "./components/Home.jsx";
+import ProductDetail from "./components/ProductDetail.jsx";
 
 const routeDefinitions = createRoutesFromElements(
   <Route path="/" element={<App />} errorElement={<ErrorPage />}>
     <Route index element={<Home />} loader={productsLoader} />
     <Route path="/home" element={<Home />} loader={productsLoader} />
     <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
+    <Route path="/contact" element={<Contact />} action={contactAction} />
     <Route path="/login" element={<Login />} />
     <Route path="/cart" element={<Cart />} />
-  </Route>
+    <Route path="/products/:productId" element={<ProductDetail />} />
+  </Route>,
 );
 
 const router = createBrowserRouter(routeDefinitions);
@@ -58,8 +67,18 @@ const router = createBrowserRouter(routeDefinitions);
   },
 ]);*/
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+     <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      draggable
+      pauseOnHover
+      theme={localStorage.getItem("theme") === "dark" ? "dark" : "light"}
+      transition={Bounce}
+    />
   </StrictMode>,
-)
+);
