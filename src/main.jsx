c1b68ab3e31a18dc4ae11256685ef170a447f762
Shiran Dinushka <shirandinushka9@gmail.com>
2026-07-02 20:47:09 +0530

    Add loader and action for profile path

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ import CheckoutForm from "./components/CheckoutForm.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import AdminOrders from "./components/Admin/AdminOrders.jsx";
 import Orders from "./components/Orders.jsx";
-import Profile from "./components/Profile.jsx";
+import Profile, { profileAction, profileLoader } from "./components/Profile.jsx";
 import Messages from "./components/Admin/Messages.jsx";
 import Register, { registerAction } from "./components/Register.jsx";
 
@@ -40,7 +40,7 @@ const routeDefinitions = createRoutesFromElements(
     <Route path="/products/:productId" element={<ProductDetail />} />
     <Route element={<ProtectedRoute />}>
       <Route path="/checkout" element={<CheckoutForm />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile" element={<Profile />} loader={profileLoader} action={profileAction} />
       <Route path="/orders" element={<Orders />} />
       <Route path="/admin/orders" element={<AdminOrders />} />
       <Route path="/admin/messages" element={<Messages />} />
