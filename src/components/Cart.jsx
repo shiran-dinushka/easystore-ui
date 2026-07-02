582b0ae0049175205282002573b8a116b3fc0143
Shiran Dinushka <shirandinushka9@gmail.com>
2026-07-02 15:06:04 +0530

    Made checkout button link to checkout page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -28,9 +28,9 @@ const { cart } = useCart();
                 Back to Products
               </Link>
               {/* Proceed to Checkout Button */}
-              <button className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition">
+              <Link to="/checkout"  className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:bg-dark dark:hover:bg-lighter transition">
                 Proceed to Checkout
-              </button>
+              </Link>
             </div>
           </>
         ) : (
